486bd955944bd9688eebab8acdd92e4bfa604352
Jason Davis <jdavis@c.ringling.edu>
2020-04-26 17:34:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(list)
+  list.collect do|attendees|
+     "Hello, my name is #{attendees}."
+  end
+end
+
+require "pry"
+def assign_rooms(attendees)
+  attendees.each_with_index.collect do|attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+
+end
+end
+
+def printer(list)
+  list.collect do|attendees|
+     "Hello, my name is #{attendees}."
+     end
+   end
